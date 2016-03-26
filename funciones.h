43b5540cb0e47fa6b6252f2a27776ae7557a39c9
CanteroAgustin <cantero.agustin@yahoo.com.ar>
2016-03-26 20:33:34 -0300

    Commit TP-1
    Primer commit del TP-1

---
+++ funciones.h
@@ -0,0 +1,83 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include <math.h>
+
+
+/**
+ * \brief Recibe dos numeros, los suma e imprime el resultado por pantalla
+ * \param operando1, operando 2 numeros a sumar
+ *
+ */
+void sumar(float operando1, float operando2){
+    float suma = 0;
+    suma=operando1+operando2;
+    printf("El resultado de la suma (A+B) es: %.2f\n",suma);
+}
+
+/**
+ * \brief Recibe dos numeros, los resta e imprime el resultado por pantalla
+ * \param operando1, operando 2 numeros a restar
+ *
+ */
+void restar(float operando1, float operando2){
+    float resta;
+    resta=operando1-operando2;
+    printf("El resultado de la resta (A-B) es: %.2f\n",resta);
+}
+
+/**
+ * \brief Recibe dos numeros, los multiplica e imprime el resultado por pantalla
+ * \param operando1, operando 2 numeros a multiplicar
+ *
+ */
+void multiplicar(float operando1, float operando2){
+    float multiplicacion;
+    multiplicacion=operando1*operando2;
+    printf("El resultado de la multiplicacion (A*B) es: %.2f\n",multiplicacion);
+}
+
+/**
+ * \brief Recibe dos numeros, los divide e imprime el resultado por pantalla
+ * \param operando1, operando 2 numeros a dividir
+ *
+ */
+void dividir(float operando1, float operando2){
+    float division;
+    if (operando2 == 0){
+        printf("No se puede dividir por 0\n");
+
+
+    }else{
+    division=operando1/operando2;
+    printf("El resultado de la division (A/B) es: %.2f\n",division);
+    }
+
+}
+
+/**
+ * \brief Recibe un numero, calcula el factorial e imprime el resultado por pantalla
+ * \param operando1, numero sobre el que se calculara el factorial
+ *
+ */
+void calcularFactorial(float operando1){
+    int factor=1;
+    int auxiliar = 0;
+    auxiliar = (int) operando1;
+    if (operando1 <= 0){
+        printf("Solo se puede calcular falctorial de un entero positivo\n");
+
+
+    }else{
+        while (auxiliar > 0)
+            {
+
+
+                factor = auxiliar * factor;
+                auxiliar--;
+            }
+            printf("El factorial de (A) es: %d\n", factor);
+    }
+
+}
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+    float operando1=0; float operando2=0;
+    while(seguir=='s')
+    {
+        do{
+        printf("-------------------------------------------------------------\n");
+        printf("1- Ingresar 1er operando (A=%.2f)\n",operando1);
+        printf("2- Ingresar 2do operando (B=%.2f)\n",operando2);
+        printf("3- Calcular la suma (A+B)\n");
+        printf("4- Calcular la resta (A-B)\n");
+        printf("5- Calcular la division (A/B)\n");
+        printf("6- Calcular la multiplicacion (A*B)\n");
+        printf("7- Calcular el factorial (A!)\n");
+        printf("8- Calcular todas las operaciones\n");
+        printf("9- Salir\n");
+        printf("-------------------------------------------------------------\n");
+        setbuf(stdin, NULL);
+        }
+        while(scanf("%d", &opcion)!= 1);
+
+
+        switch(opcion)
+        {
+            case 1:
+                    do{
+                    printf("Ingrese el primer numero \n");
+                    setbuf(stdin, NULL);
+                    }
+                    while(scanf("%f", &operando1)!= 1);
+                break;
+            case 2:
+                    do{
+                    printf("Ingrese el segundo numero \n");
+                    setbuf(stdin, NULL);
+                    }
+                    while(scanf("%f", &operando2)!= 1);
+                break;
+            case 3:
+                    printf("-------------------------------------------------------------\n");
+                    sumar(operando1,operando2);
+                    printf("-------------------------------------------------------------\n");
+                break;
+            case 4:
+                    printf("-------------------------------------------------------------\n");
+                    restar(operando1,operando2);
+                    printf("-------------------------------------------------------------\n");
+                break;
+            case 5:
+                    printf("-------------------------------------------------------------\n");
+                    dividir(operando1,operando2);
+                    printf("-------------------------------------------------------------\n");
+                break;
+            case 6:
+                    printf("-------------------------------------------------------------\n");
+                    multiplicar(operando1,operando2);
+                    printf("-------------------------------------------------------------\n");
+                break;
+            case 7:
+                    printf("-------------------------------------------------------------\n");
+                    calcularFactorial(operando1);
+                    printf("-------------------------------------------------------------\n");
+                break;
+            case 8:
+                    printf("-------------------------------------------------------------\n");
+                    sumar(operando1,operando2);
+                    restar(operando1,operando2);
+                    dividir(operando1,operando2);
+                    multiplicar(operando1,operando2);
+                    calcularFactorial(operando1);
+                    printf("-------------------------------------------------------------\n");
+                break;
+            case 9:
+                seguir = 'n';
+                break;
+            default:
+                    printf("Ingrese una opcion valida 1-9\n");
+                break;
+        }
+
+
+    }
+    return 0;
+}
