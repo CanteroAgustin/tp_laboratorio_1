d05f72db20248784e833b4383c7e1ef442bedfa1
CanteroAgustin <cantero.agustin@yahoo.com.ar>
2016-04-08 10:47:19 -0300

    Added files via upload

--- main.c
+++ main.c
@@ -2,93 +2,88 @@
 #include <stdlib.h>
 #include "funciones.h"
 
-//v1.0
+int imprimirMenu(float operando1, float operando2);
+void sumar(float operando1, float operando2, int flagIngresoOperando1, int flagIngresoOperando2);
+void restar(float operando1, float operando2, int flagIngresoOperando1, int flagIngresoOperando2);
+void multiplicar(float operando1, float operando2, int flagIngresoOperando1, int flagIngresoOperando2);
+void dividir(float operando1, float operando2, int flagIngresoOperando1, int flagIngresoOperando2);
+void calcularFactorial(int operando1, int flagIngresoOperando1);
 
 int main()
 {
     char seguir='s';
-    int opcion=0;
-    float operando1=0; float operando2=0;
+    float operando1=0;
+    float operando2=0;
+    int flagIngresoOperando1=0;
+    int flagIngresoOperando2=0;
+
     while(seguir=='s')
     {
-        do{
-        printf("-------------------------------------------------------------\n");
-        printf("1- Ingresar 1er operando (A=%.2f)\n",operando1);
-        printf("2- Ingresar 2do operando (B=%.2f)\n",operando2);
-        printf("3- Calcular la suma (A+B)\n");
-        printf("4- Calcular la resta (A-B)\n");
-        printf("5- Calcular la division (A/B)\n");
-        printf("6- Calcular la multiplicacion (A*B)\n");
-        printf("7- Calcular el factorial (A!)\n");
-        printf("8- Calcular todas las operaciones\n");
-        printf("9- Salir\n");
-        printf("-------------------------------------------------------------\n");
-        setbuf(stdin, NULL);
-        }
-        while(scanf("%d", &opcion)!= 1);
-
-
-        switch(opcion)
+        switch(imprimirMenu(operando1, operando2))
         {
             case 1:
                     do{
-                    printf("-------------------------------------------------------------\n");
-                    printf("Ingrese el primer numero \n");
-                    setbuf(stdin, NULL);
+                        printf("*************************************************\n");
+                        printf("Ingrese el primer numero \n");
+                        setbuf(stdin, NULL);
                     }
                     while(scanf("%f", &operando1)!= 1);
+                    flagIngresoOperando1=1;
                 break;
             case 2:
                     do{
-                    printf("-------------------------------------------------------------\n");
-                    printf("Ingrese el segundo numero \n");
-                    setbuf(stdin, NULL);
+                        printf("*************************************************\n");
+                        printf("Ingrese el segundo numero \n");
+                        setbuf(stdin, NULL);
                     }
                     while(scanf("%f", &operando2)!= 1);
+                    flagIngresoOperando2=1;
                 break;
             case 3:
-                    printf("-------------------------------------------------------------\n");
-                    sumar(operando1,operando2);
-                    printf("-------------------------------------------------------------\n");
+                    printf("*************************************************\n");
+                    sumar(operando1,operando2,flagIngresoOperando1,flagIngresoOperando2);
+                    printf("*************************************************\n");
                 break;
             case 4:
-                    printf("-------------------------------------------------------------\n");
-                    restar(operando1,operando2);
-                    printf("-------------------------------------------------------------\n");
+                    printf("*************************************************\n");
+                    restar(operando1, operando2, flagIngresoOperando1, flagIngresoOperando2);
+                    printf("*************************************************\n");
                 break;
             case 5:
-                    printf("-------------------------------------------------------------\n");
-                    dividir(operando1,operando2);
-                    printf("-------------------------------------------------------------\n");
+                    printf("*************************************************\n");
+                    dividir(operando1,operando2, flagIngresoOperando1, flagIngresoOperando2);
+                    printf("*************************************************\n");
                 break;
             case 6:
-                    printf("-------------------------------------------------------------\n");
-                    multiplicar(operando1,operando2);
-                    printf("-------------------------------------------------------------\n");
+                    printf("*************************************************\n");
+                    multiplicar(operando1,operando2, flagIngresoOperando1, flagIngresoOperando2);
+                    printf("*************************************************\n");
                 break;
             case 7:
-                    printf("-------------------------------------------------------------\n");
-                    calcularFactorial(operando1);
-                    printf("-------------------------------------------------------------\n");
+                    printf("*************************************************\n");
+                    operando1 = (int) operando1;
+                    calcularFactorial(operando1, flagIngresoOperando1);
+                    printf("*************************************************\n");
                 break;
             case 8:
-                    printf("-------------------------------------------------------------\n");
-                    sumar(operando1,operando2);
-                    restar(operando1,operando2);
-                    dividir(operando1,operando2);
-                    multiplicar(operando1,operando2);
-                    calcularFactorial(operando1);
-                    printf("-------------------------------------------------------------\n");
+                    printf("*************************************************\n");
+                    sumar(operando1,operando2, flagIngresoOperando1, flagIngresoOperando2);
+                    restar(operando1,operando2, flagIngresoOperando1, flagIngresoOperando2);
+                    multiplicar(operando1,operando2, flagIngresoOperando1, flagIngresoOperando2);
+                    dividir(operando1,operando2, flagIngresoOperando1, flagIngresoOperando2);
+                    operando1 = (int) operando1;
+                    calcularFactorial(operando1, flagIngresoOperando1);
+                    printf("*************************************************\n");
                 break;
             case 9:
                 seguir = 'n';
                 break;
             default:
-                    printf("Ingrese una opcion valida 1-9\n");
+                    printf("*************************************************\n");
+                    printf("Ingrese una opcion valida (1-9)\n");
+                    printf("*************************************************\n");
                 break;
         }
-
-
     }
     return 0;
 }
